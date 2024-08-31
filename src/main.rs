c3e1897f74f3af91bb02116bee431a9f8249e1f7
jeisaRaja <jeisa.raja.jrt@gmail.com>
2024-08-31 16:28:19 +0700

    feat: add initiate_json_store to save tasks

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,54 @@
 use chrono::{DateTime, Local, Utc};
 use clap::Parser;
+use serde::{Deserialize, Serialize};
+use std::{
+    fs::{self},
+    io,
+    path::Path,
+};
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 enum TaskStatus {
     Done,
     Progress,
     Todo,
 }
 
-fn local_time() -> DateTime<Local> {
-    let utc_time: DateTime<Utc> = Utc::now();
-    let local_time: DateTime<Local> = utc_time.with_timezone(&Local);
-
-    local_time
-}
-
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 struct Task {
     id: i8,
     name: String,
     status: TaskStatus,
     created_at: DateTime<Local>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Tasks {
+    tasks: Vec<Task>,
+}
+
+fn initiate_json_store() -> io::Result<Tasks> {
+    let path = Path::new("tasks.json");
+    if path.exists() {
+        println!("tasks.json exists!");
+        let json_file = fs::read_to_string(path)?;
+        let tasks: Tasks = serde_json::from_str(&json_file)?;
+
+        Ok(tasks)
+    } else {
+        println!("tasks.json does not exist");
+        let empty_tasks = Tasks { tasks: vec![] };
+        let json_data = serde_json::to_string_pretty(&empty_tasks)?;
+        fs::write(path, json_data)?;
+
+        Ok(empty_tasks)
+    }
+}
+
+fn local_time() -> DateTime<Local> {
+    let utc_time: DateTime<Utc> = Utc::now();
+    let local_time: DateTime<Local> = utc_time.with_timezone(&Local);
+    local_time
 }
 
 #[derive(Parser, Debug)]
@@ -44,6 +72,7 @@ fn main() {
                 created_at: local_time(),
                 status: TaskStatus::Todo,
             };
+
             println!("{:?}", task)
         }
         _ => {
@@ -51,4 +80,5 @@ fn main() {
         }
     }
     local_time();
+    let _tasks = initiate_json_store();
 }
