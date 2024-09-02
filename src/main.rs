366a754b087b4dcec1dc132b96101fa974c4d6d4
jeisaRaja <jeisa.raja.jrt@gmail.com>
2024-09-02 13:53:15 +0700

    refactor: add modules for task

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,118 @@
 use chrono::{DateTime, Local, Utc};
 use clap::Parser;
-use core::fmt;
-use serde::{Deserialize, Serialize};
 use std::{fs, io, path::Path};
 
-#[derive(Debug, Deserialize, Serialize)]
-enum TaskStatus {
-    Done,
-    Progress,
-    Todo,
-}
-impl fmt::Display for TaskStatus {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let status = match self {
-            Self::Done => "[✔]",
-            Self::Progress => "[~]",
-            Self::Todo => "[ ]",
-        };
-        write!(f, "{}", status)
-    }
-}
+mod task {
 
-#[derive(Debug, Deserialize, Serialize)]
-struct Task {
-    id: i8,
-    name: String,
-    status: TaskStatus,
-    created_at: DateTime<Local>,
-}
+    use crate::{local_time, write_to_json_store};
+    use chrono::{DateTime, Local};
+    use core::fmt;
+    use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
-struct Tasks {
-    tasks: Vec<Task>,
-}
-
-impl Tasks {
-    fn add_task(&mut self, task: Task) {
-        self.tasks.push(task);
-        write_to_json_store(&self).unwrap();
+    #[derive(Debug, Deserialize, Serialize)]
+    pub enum TaskStatus {
+        Done,
+        Progress,
+        Todo,
     }
 
-    fn delete_task(&mut self, id: i8) {
-        println!("deleting task {}", id);
-        self.tasks.retain(|item| item.id != id);
-        write_to_json_store(&self).unwrap();
-    }
-
-    fn id(&self) -> i8 {
-        let len_tasks = self.tasks.len();
-        if len_tasks == 0 {
-            return 1;
+    impl core::fmt::Display for TaskStatus {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            let status = match self {
+                Self::Done => "[✔]",
+                Self::Progress => "[~]",
+                Self::Todo => "[ ]",
+            };
+            write!(f, "{}", status)
         }
-        return self.tasks[len_tasks - 1].id + 1;
     }
 
-    fn mark_progress(&mut self, id: i8) {
-        for task in self.tasks.iter_mut() {
-            if task.id == id {
-                task.status = TaskStatus::Progress;
+    #[derive(Debug, serde::Deserialize, serde::Serialize)]
+    pub struct Task {
+        id: i8,
+        name: String,
+        status: TaskStatus,
+        created_at: DateTime<Local>,
+    }
+
+    impl Task {
+        pub fn new(name: String, tasks: &Tasks) -> Task {
+            return Task {
+                id: tasks.id(),
+                name,
+                created_at: local_time(),
+                status: TaskStatus::Todo,
+            };
+        }
+    }
+
+    #[derive(Debug, Deserialize, Serialize)]
+    pub struct Tasks {
+        tasks: Vec<Task>,
+    }
+
+    impl Tasks {
+        pub fn new() -> Tasks {
+            return Tasks { tasks: vec![] };
+        }
+        pub fn add_task(&mut self, task: Task) {
+            self.tasks.push(task);
+            write_to_json_store(&self).unwrap();
+        }
+
+        pub fn delete_task(&mut self, id: i8) {
+            println!("deleting task {}", id);
+            self.tasks.retain(|item| item.id != id);
+            write_to_json_store(&self).unwrap();
+        }
+
+        pub fn id(&self) -> i8 {
+            let len_tasks = self.tasks.len();
+            if len_tasks == 0 {
+                return 1;
+            }
+            return self.tasks[len_tasks - 1].id + 1;
+        }
+
+        pub fn mark_progress(&mut self, id: i8) {
+            for task in self.tasks.iter_mut() {
+                if task.id == id {
+                    task.status = TaskStatus::Progress;
+                }
+            }
+            write_to_json_store(&self).unwrap();
+        }
+
+        pub fn list(&self) {
+            for task in self.tasks.iter() {
+                println!("{}", task);
             }
         }
-        write_to_json_store(&self).unwrap();
     }
 
-    fn list(&self) {
-        for task in self.tasks.iter() {
-            println!("{}", task);
+    impl fmt::Display for Task {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            let desc_width = 20;
+            write!(
+                f,
+                " {}. {:<desc_width$} {}",
+                self.id, self.name, self.status
+            )
         }
     }
 }
 
-impl fmt::Display for Task {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let desc_width = 20;
-        write!(
-            f,
-            " {}. {:<desc_width$} {}",
-            self.id, self.name, self.status
-        )
-    }
-}
-
-fn initiate_json_store() -> io::Result<Tasks> {
+fn initiate_json_store() -> io::Result<task::Tasks> {
     let path = Path::new("tasks.json");
     if path.exists() {
         println!("tasks.json exists!");
         let json_file = fs::read_to_string(path)?;
-        let tasks: Tasks = serde_json::from_str(&json_file)?;
+        let tasks: task::Tasks = serde_json::from_str(&json_file)?;
 
         Ok(tasks)
     } else {
         println!("tasks.json does not exist");
-        let empty_tasks = Tasks { tasks: vec![] };
+        let empty_tasks = task::Tasks::new();
         let json_data = serde_json::to_string_pretty(&empty_tasks)?;
         fs::write(path, json_data)?;
 
@@ -99,7 +120,7 @@ fn initiate_json_store() -> io::Result<Tasks> {
     }
 }
 
-fn write_to_json_store(contents: &Tasks) -> io::Result<()> {
+fn write_to_json_store(contents: &task::Tasks) -> io::Result<()> {
     let path = Path::new("tasks.json");
     let str_contents = serde_json::to_string_pretty(contents)?;
     fs::write(path, str_contents)?;
@@ -136,14 +157,8 @@ fn main() {
                 println!("Provide the task!");
                 return;
             }
-            let id = tasks.id();
-            let task = Task {
-                name: args.val.expect("Should be a string"),
-                id,
-                created_at: local_time(),
-                status: TaskStatus::Todo,
-            };
-            tasks.add_task(task);
+            let task_add = task::Task::new(args.val.expect("a string"), &tasks);
+            tasks.add_task(task_add);
         }
         Some("delete") => {
             if args.val == None {
